ef318bed4a2cad84425629738a76ba6bea837419
Jun Murakami <126404131+Jun-Murakami@users.noreply.github.com>
2024-04-01 03:47:17 +0900

    パッケージ.jsonに依存関係を追加しました

---
+++ android/app/src/main/java/com/wlsib/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wlsib.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wlsib.app',
+  appName: 'ﾚﾝｽﾞ何持ってく?',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
